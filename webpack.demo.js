224503869e833b8bcaf1eb1933c955d570090bd4
yoanngueny <yoann.gueny@gmail.com>
2017-04-28 15:19:45 +0200

    fix webpack config error

--- webpack.demo.js
+++ webpack.demo.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function(env) {
     return require('./webpack/'+env+'.js')({
-        host: 'http://projects.dev/YOANN/yg-webapp/dist',
+        host: 'http://your-host',
         app: path.resolve(__dirname, 'app'),
         dist: path.resolve(__dirname, 'dist')
     });
